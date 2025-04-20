626b2b573af3d2725e228207b6a5cff37c45a70d
Akash Kumar <akashkumar.cs27@gmail.com>
2025-04-20 18:28:52 +0530

    update

--- src/forecast.py
+++ src/forecast.py
@@ -8,16 +8,19 @@ if 'y' not in df.columns:
     raise ValueError("Column 'y' not found in the input data. Check preprocessing.")
 if 'AQI' in df.columns:
     df = df.rename(columns={'AQI': 'y'})
-df['y'] = df['y'].clip(lower=0) 
+df['y'] = df['y'].clip(lower=0,upper=500)
 
 model = Prophet(
     yearly_seasonality=True,
     weekly_seasonality=True,
     daily_seasonality=True,
-    seasonality_mode='additive'
+    seasonality_mode='additive',
+    changepoint_prior_scale=0.1,
+    changepoint_range=0.9
 )
 
 model.fit(df)
+
 with open('models/trained_model.pkl', 'wb') as f:
     pickle.dump(model, f)
 
@@ -25,6 +28,7 @@ last_date = pd.to_datetime(df['ds'].iloc[-1])
 future = pd.DataFrame({'ds': pd.date_range(start=last_date + pd.Timedelta(hours=1), periods=17520, freq='h')})
 
 forecast = model.predict(future)
+forecast['yhat'] = forecast['yhat'].clip(lower=0, upper=500)
 forecast.to_csv("outputs/forecasts/aqi_forecast_sohna_next_year_long.csv", index=False)
 
 forecast_pivot = forecast[['ds', 'yhat']].copy()
@@ -36,6 +40,6 @@ pivot_table.index.name = None
 
 pivot_table.to_csv("outputs/forecasts/aqi_forecast_sohna_next_year.csv", index=False)
 
-print("Forecast shape:", pivot_table.shape)
-print("Min forecasted AQI:", pivot_table.iloc[:, 1:].min().min())
-print("Max forecasted AQI:", pivot_table.iloc[:, 1:].max().max())
+print("Forecast shape:", forecast.shape)
+print("Min forecasted AQI:", forecast['yhat'].min())
+print("Max forecasted AQI:", forecast['yhat'].max())
--- src/visualize.py
+++ src/visualize.py
@@ -9,7 +9,6 @@ forecast['ds'] = pd.to_datetime(forecast['ds'])
 with open("models/trained_model.pkl", "rb") as file:
     model = pickle.load(file)
 
-# Create and save the interactive forecast plot
 fig = plot_plotly(model, forecast)
 fig_components = plot_components_plotly(model, forecast)
 fig.write_html("outputs/forecasts/aqi_forecast_plot.html")
